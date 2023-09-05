e5cea0b6de1f8009e6a6f2122c9db8a329cf028f
dekopon21020014 <renueta@icloud.com>
2023-09-05 11:15:47 +0900

    折れ線グラフでのリアクションの可視化(同期通信)ができた

--- app/controllers/user_reactions_controller.rb
+++ app/controllers/user_reactions_controller.rb
@@ -1,16 +1,16 @@
 class UserReactionsController < ApplicationController
   def new
-    @reactions = Reaction.all
     @user_reaction = UserReaction.new
+    @reactions     = Reaction.all
   end
 
   def index
     @user_reactions = UserReaction.all
+    @reactions      = Reaction.all
   end
 
   def create
     @user_reaction = UserReaction.new
-    @user_reaction.user_id = params[:user_id].to_i
     @user_reaction.reaction_id = params[:reaction_id].to_i
     if @user_reaction.save
       redirect_to new_user_reaction_path
@@ -20,6 +20,6 @@ class UserReactionsController < ApplicationController
   end
 
   def user_reaction_params
-    params.require(:user_reaction).permit(:user_id, :reaction_id)
+    params.require(:user_reaction).permit(:reaction_id)
   end
 end
--- db/migrate/20230831063439_create_user_reactions.rb
+++ db/migrate/20230831063439_create_user_reactions.rb
@@ -2,7 +2,6 @@ class CreateUserReactions < ActiveRecord::Migration[6.1]
   def change
     create_table :user_reactions do |t|
       t.integer :reaction_id
-      t.integer :user_id
       t.timestamps
     end
   end
