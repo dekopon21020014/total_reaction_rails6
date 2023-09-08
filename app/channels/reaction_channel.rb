8954b9eadc09de4269c733bde37b3994d3b0e4f9
dekopon21020014 <renueta@icloud.com>
2023-09-08 13:12:18 +0900

    ユーザがボタンを押すと，発表者側のボタンが変化する

--- app/channels/reaction_channel.rb
+++ app/channels/reaction_channel.rb
@@ -1,10 +1,29 @@
 class ReactionChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
-    stream_from "reaction_channel"
+    stream_from "reaction_channel_#{params['room_id']}"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  def send_reaction(data)
+    # Chatにデータを保存
+    UserReaction.create(
+      reaction_id: data['reaction'] # keyは多分name属性
+    )
+
+    # 部分テンプレートをWebSocket経由で送り出す。
+    #   render_messageで部分テンプレートに文字を埋め込みmessageとして送り出している。
+    ActionCable.server.broadcast "reaction_channel_2", {reaction: render_reaction(data)}
+  end
+
+  private
+
+  def render_reaction(data)
+    # renderではなくrendererに注意してください
+    #   rendererは、コントローラの制約を受けずに任意のビューテンプレートをレンダリングします
+    ApplicationController.renderer.render(partial: 'user_reactions/reaction', locals: {reaction: data})
   end
 end
--- app/javascript/channels/reaction_channel.js
+++ app/javascript/channels/reaction_channel.js
@@ -1,5 +1,5 @@
 import consumer from "./consumer"
-
+/*
 consumer.subscriptions.create("ReactionChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
@@ -18,3 +18,50 @@ consumer.subscriptions.create("ReactionChannel", {
     newMessage.value='';
   }
 });
+*/
+
+
+$(document).on('turbolinks:load', function () {
+
+  const reactions = $('#reactions') // reactionsのエレメント
+
+  // ルームIDを#reactions内のdata-room-idから取得し、
+  // chats_channel.rbでも使えるようにする
+  const room = consumer.subscriptions.create(
+      {channel: "ReactionChannel", room_id: reactions.data('room_id')}, {
+
+          connected() {
+              // 未使用
+          },
+
+          disconnected() {
+              // 未使用
+          },
+
+          received(data) {
+              // 受け取ったデータを追加する
+              //  dataには、reactions_channel.rbから送られるパラメーター名に
+              //  部分テンプレートに文字が埋め込まれているものが送られてくるので
+              //  配列からmessageを抜き出し追加している。
+              console.log(data);
+              // reactions.append(data['reaction'])
+              reactions.html('<div>こんなになっちゃった</div>')
+          },
+
+          send_reaction: function (reaction) {
+              return this.perform('send_reaction', {reaction: reaction})
+          }
+      });
+
+  // 入力フォームの制御
+  $(document).ready(function () {
+      // 入力エリアのエレメント
+      const buttonElement = $('#reaction')
+      // 入力エリアのEnterKey検出
+      buttonElement.click(function (event) {
+              // send_reactionを呼び出し
+              room.send_reaction(buttonElement.val());
+              event.preventDefault();
+      })
+  })
+})
