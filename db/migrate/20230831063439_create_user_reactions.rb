710eb2f4cbccf943a8f1b6e5f57be9ddb143b866
dekopon21020014 <renueta@icloud.com>
2024-01-12 23:30:20 +0900

    migrationファイルの編集

--- db/migrate/20230831063439_create_user_reactions.rb
+++ db/migrate/20230831063439_create_user_reactions.rb
@@ -2,6 +2,7 @@ class CreateUserReactions < ActiveRecord::Migration[6.1]
   def change
     create_table :user_reactions do |t|
       t.integer :reaction_id
+      t.integer :slide_id
       t.timestamps
     end
   end
